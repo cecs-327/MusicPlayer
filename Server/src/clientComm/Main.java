f7cb9668d85931dac0cc4efdd9d9a57c9fc3971c
ChrisMeyer7088 <chrismeyer175@gmail.com>
2019-04-09 13:42:38 -0700

    Bug fixes

--- Server/src/clientComm/Main.java
+++ Server/src/clientComm/Main.java
@@ -46,9 +46,8 @@ public class Main {
 
         // Main player = new Main();
         // player.mp3play(490183L, proxy);
-    	pathHolder.dfs = new DFS(pathHolder.portNum);
-		//Make sure DFS where files are kept is always started on the same port number as portToJoin
-		pathHolder.dfs.join("127.0.0.1", pathHolder.portToJoin);
+//    	pathHolder.dfs = new DFS(pathHolder.portNum);
+//		pathHolder.dfs.join("127.0.0.1", pathHolder.portToJoin);
         SocketLayer sl = new SocketLayer();
         sl.run();
         
